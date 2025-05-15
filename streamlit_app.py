0fd1c7e7b2ceef59cdb0311d71e28e25e4b86be5
azkarazka <namiraazka2907@gmail.com>
2025-05-15 13:53:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 st.title("🎈 My new app")
+st.image("48084167-E3FE-464B-A379-3BA1AA6F9204.jpeg", width=200)
+st.subheader("Thifazkarazkas dog")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+  "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
